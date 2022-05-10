ded45af7bd0ef4cdaedd37ebda4fb903a8f14a3d
Leyla <90026779+LeylAde@users.noreply.github.com>
2022-05-10 15:02:06 +0200

    Update expected_util_nagents.py

--- simple_teams/expected_util_nagents.py
+++ simple_teams/expected_util_nagents.py
@@ -27,12 +27,10 @@ def calculate_pre_set_n_agents(n, m, game_matrix): # omega = tr-probability, n =
             # PROBLEM! at the moment shaped_profile does not have enough entries, for each new non_team_cell we will substitute the old, as A will be computed under the same index!
     return shaped_profiles # for each profile (indiv, tr) we will have a list of tuples containing profile combination of this profile (depending on whether agents play individual or team strategy) and number of team-reasoners
 
-def calculate_utils_n_agents(omega, n, m, game_matrix): # omega = tr-probability, n = number agents, m = number action per agent, game_matrix is matrix with r cells and n-tuples per cell
-    all_coordinates_in_game = list(
-        itertools.product(*[list(range(x)) for x in game_matrix.shape])
-    )
-    shaped_utils = np.zeros((2**n, m, m))
-    for index, profile_list in enumerate(calculate_pre_set_n_agents(omega, n, m, game_matrix)): # for a list of tuples
+def calculate_utils_n_agents(omega, n, game_matrix): # omega = tr-probability, n = number agents, m = number action per agent, game_matrix is matrix with r cells and n-tuples per cell
+    pre_set = calculate_pre_set_n_agents(n, game_matrix)
+    shaped_utils = np.zeros(len(pre_set)) # for now this is a one dimensional array TODO: Check what format gambit needs to compute NE
+    for index, profile_list in enumerate(pre_set): # for a list of tuples
         util = 0
         for profile in profile_list: # for one tuple
             util = util + ((omega**profile[1])*((1-omega)**(n-profile[1]))*game_matrix[profile[0]])
