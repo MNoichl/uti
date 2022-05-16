b9232671ed9e9b12879d6fb016a16725cc67df78
Leyla <90026779+LeylAde@users.noreply.github.com>
2022-05-16 14:21:39 +0200

    n agents team reasoning
    solved the last error: making sure everything has the correct shape such that the utility list can be filled correctly

--- simple_teams/expected_util_nagents.py
+++ simple_teams/expected_util_nagents.py
@@ -1,38 +1,56 @@
 import math
 import itertools
+from typing import List, Any
+
 import numpy as np
 
 # DISCLAIMER: this is very much work in progress
 
 
-def calculate_pre_set_n_agents(n, game_matrix): # omega = tr-probability, n = number agents, m = number action per agent, game_matrix is matrix with r cells and n-tuples per cell
-    all_coordinates_in_game = list(
-        itertools.product(*[list(range(x)) for x in game_matrix.shape])
-    ) # Why is there no 'repeat' variable? Is this already the correct length?
-    shaped_profiles = [] # np.zeros(m**(2*n)) # list; entries will be A's; how many A's are there? Should I create a list of zero's rather than an empty list?
-    for index, team_plays in enumerate(all_coordinates_in_game): # index, tuple of n action for team reasoners
-        for non_team_cell in all_coordinates_in_game: # tuple of n action for non-team reasoners
+def calculate_utils_n_agents(omega, n, m, game_matrix, player): # omega = tr-probability, n = number agents, m = number action per agent, game_matrix is matrix with r cells and n-tuples per cell
+    #all_coordinates_in_game = list(itertools.product(game_matrix.shape, repeat=n)) #
+    g = list(itertools.product(*[list(range(x)) for x in game_matrix.shape]))
+    all_coordinates_in_game = []
+    for profile in g:
+        new_profile = profile[:-1]
+        if all( i != new_profile for i in all_coordinates_in_game):
+            all_coordinates_in_game.append(new_profile)
+        else:
+            continue
+    # print('all coordinates:', all_coordinates_in_game)
+    # all_coordinates_in_game = list(itertools.product(*[list(range(x)) for x in game_matrix.shape]))
+    #all_coordinates_in_game = list(
+    #    itertools.product(*[list(range(x)) for x in game_matrix.shape])
+    #) # Why is there no 'repeat' variable? Is this already the correct length?
+    a = list(itertools.repeat(m, n)) # creates list of n times number m, indicating how many actions (m) each agent (in total n agents) has
+    a.insert(0, m ** n) # adds number m**n on zero's position of a, this is now a tuple indicating how many actions the team and each agent has
+    a.append(n)
+    shaped_utils = np.zeros(a)  # this is now an n+1 dimensional table in which each cell represents one possible profile; n dimensions have length m (each agent has m actions) the last dimension has length m**n (team has m**n actions)
+    for index, team_plays in enumerate(all_coordinates_in_game): # index, list of n action for team reasoners
+        team_plays_list = list(team_plays)
+        for non_team_cell in all_coordinates_in_game: # list of n actions for individual reasoners
+            non_team_cell_list = list(non_team_cell)
         # are team_play and non_team_cell tuples or lists? We need lists! If tuples: convert!
             k = 0
             A = [] # will have 2^n entries, which are n-tuples; all combination of actions when each player is team reasoning or not
             while k <= n:
-                sub_profiles = list(itertools.combinations(list(range(len((non_team_cell)))), k)) # gives all possible sets of indeces of actions from profile non_team_cell with cardinality k
-                for subset in sub_profiles:
-                    profile_copy = non_team_cell.copy()
+                sub_profiles = list(itertools.combinations(list(range(len(non_team_cell_list))), k)) # gives all possible sets of indeces of actions from profile non_team_cell with cardinality k
+                for subset in sub_profiles: # subset contains indeces of all current team reasoners
+                    profile_copy = non_team_cell_list.copy()
                     for j in subset:
-                        profile_copy[j] = team_plays[j]
-                    A.append([profile_copy, k])
+                        profile_copy[j] = team_plays_list[j] # substituting individual actions with team actions for team reasoners
+                    A.append([profile_copy, k]) # saves profile-combinations with numbers of team reasoners respectively
                 k += 1
-            shaped_profiles.append(A) # A is a list of tuples containing a n-tuple (profile) and integer (number of team reasoners)
-            # PROBLEM! at the moment shaped_profile does not have enough entries, for each new non_team_cell we will substitute the old, as A will be computed under the same index!
-    return shaped_profiles # for each profile (indiv, tr) we will have a list of tuples containing profile combination of this profile (depending on whether agents play individual or team strategy) and number of team-reasoners
+            util = 0
+            # print('all possible profiles:', A)
+            for profile in A:  # for one tuple
+                # print(type(profile[0]))
+                # print('position in game:', game_matrix[tuple(profile[0])])
+                # print('util in game:', game_matrix[tuple(profile[0])][0])
+                util = util + ((omega ** profile[1]) * ((1 - omega) ** (n - profile[1])) * game_matrix[tuple(profile[0])][player]) # calculating expected utility for team_play and non_team_cells, i.e. considering each profile in A multiplying the probability (depending on how many team reasoners) and its utility
+            shaped_utils[index, non_team_cell] = util  # where do I get the index from?
+    return shaped_utils # for each profile (indiv, tr) we will have a list of tuples containing profile combination of this profile (depending on whether agents play individual or team strategy) and number of team-reasoners
 
-def calculate_utils_n_agents(omega, n, game_matrix): # omega = tr-probability, n = number agents, m = number action per agent, game_matrix is matrix with r cells and n-tuples per cell
-    pre_set = calculate_pre_set_n_agents(n, game_matrix)
-    shaped_utils = np.zeros(len(pre_set)) # for now this is a one dimensional array TODO: Check what format gambit needs to compute NE
-    for index, profile_list in enumerate(pre_set): # for a list of tuples
-        util = 0
-        for profile in profile_list: # for one tuple
-            util = util + ((omega**profile[1])*((1-omega)**(n-profile[1]))*game_matrix[profile[0]])
-        shaped_utils[index] = util # where do I get the index from?
-    return shaped_utils
+
+# game = np.array([[[[3,0,0], [0,0,1]], [[0,0,1], [0,0,1]]], [[[3,0,0], [0,0,1]],[[3,0,0], [0,0,1]]]])
+# print(calculate_utils_n_agents(0.6, 3, 2, game, 0))
---
+++ simple_teams/n_agents_reasoning.py
@@ -0,0 +1,30 @@
+import numpy as np
+import itertools
+import pygambit
+from fractions import Fraction
+import os
+from .expected_util_nagents import calculate_utils_n_agents
+
+def team_reasoning_n_agents(m, n, game, omega): # the game input is a list of n matrices, one for each player with dimensions m**n (m number of actions), omega is number indicating team reasoners fraction
+    utils = []
+    i = 0 # looping thorugh all agents
+    team_util = 0
+    while i < n:
+        util_agent_i = calculate_utils_n_agents(omega, n, m, game, i)
+        utils.append(util_agent_i)
+        team_util = team_util + util_agent_i # summing over all utils of all agents, to build average
+        i += 1
+    utils.insert(0, team_util / n) # list of all utils, teams at position 0, agent i's on position i (starting from 1)
+    a = list(itertools.repeat(m, n)) # listing n times the number m
+    g = pygambit.Game.new_table(a) # https://gambitproject.readthedocs.io/en/latest/pyapi.html
+    for ix, payoffs in enumerate(utils): # ix takes int value from 0 to n, payoffs
+        for iy, value in np.ndenumerate(payoffs): # iy is a n+1 tuple indicating the played strategies, values are the utils
+            g[iy][ix] = Fraction(str(value))
+
+    f = open("current_game.nfg", "w")
+    f.write(g.write())
+    f.close()
+
+    return 'game is now in gambit'
+
+
